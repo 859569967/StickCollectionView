69ae2588adf457755201997c6772bff816241321
RanYeee <IMac@RanydeiMac.local>
2016-03-10 16:27:53 +0800

    first commit

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StickCollectionView
+//
+//  Created by IMac on 16/3/10.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StickCollectionView
+//
+//  Created by IMac on 16/3/10.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
